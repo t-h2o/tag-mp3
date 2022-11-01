dc1b92a2b65ecb1b526d1bd2d4f38f4d3cee10ca
|°_°| <grivel.theo@protonmail.com>
2022-11-01 22:33:46 +0100

    Find an scanf example
    https://www.reddit.com/r/bash/comments/gxygww/why_isnt_there_scanf_or_equivalent_in_bash/

---
+++ tag.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pattern="Fan: (.+) rpm CPU die temperature: (.+) C"
+if [[ "Fan: fann rpm CPU die temperature: 42 C" =~ $pattern ]]; then
+    fan=${BASH_REMATCH[1]}
+    cpu=${BASH_REMATCH[2]}
+fi
+echo "$fan" "$cpu"
