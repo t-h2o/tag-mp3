57b13e7f3eb40fa7908429cc20009cdd0a09f9e7
|°_°| <grivel.theo@protonmail.com>
2022-11-02 01:30:40 +0100

    Tag the track number and the title

--- tag.sh
+++ tag.sh
@@ -5,4 +5,5 @@ for filename in ./*\.mp3; do
 	track=$(echo "${filename}" | awk '{printf "%d", substr($1, 3)}')
 	title=$(echo "${filename}" | awk '{i = 3} {while (i + 1 <= NF ) {printf "%s ", $i; i++}} {printf "%s", substr($i, 0, length($i) - 4)}')
 	printf "Track: %s\tTitle: %s\n" "${track}" "${title}"
+	mp3info -t "${title}" -n "${track}" "${filename}"
 done
