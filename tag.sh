014a48d087e143656763cdf7155a93523041f475
|°_°| <grivel.theo@protonmail.com>
2022-11-02 01:26:10 +0100

    Save track and title in variable

--- tag.sh
+++ tag.sh
@@ -2,7 +2,7 @@
 
 for filename in ./*\.mp3; do
 
-	echo "${filename}"
-	echo "${filename}" | awk '{printf "Track number: %d", substr($1, 3)}  {printf "\n"}'
-	echo "${filename}" | awk '{printf "Titel: " }{i = 3} {while (i + 1 <= NF ) {printf "%s ", $i; i++}} {printf "%s\n", substr($i, 0, length($i) - 4)}'
+	track=$(echo "${filename}" | awk '{printf "%d", substr($1, 3)}')
+	title=$(echo "${filename}" | awk '{i = 3} {while (i + 1 <= NF ) {printf "%s ", $i; i++}} {printf "%s", substr($i, 0, length($i) - 4)}')
+	printf "Track: %s\tTitle: %s\n" "${track}" "${title}"
 done
